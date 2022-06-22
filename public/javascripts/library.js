eaf3adfae8f1bf6ced1415153069e1001ebfcb0c
ccolyer <charbiecolyer@gmail.com>
2022-06-22 21:26:05 +1000

    Add basic list functionality

--- public/javascripts/library.js
+++ public/javascripts/library.js
@@ -1,47 +1,46 @@
 // -- HTML elements -- //
-const libraryForm = document.getElementById('libraryForm');
-const titleInput = document.getElementById('title');
-const urlInput = document.getElementById('url');
-const readingsList = document.getElementById('readingsList');
+const libraryForm = document.getElementById("libraryForm");
+const titleInput = document.getElementById("title");
+const urlInput = document.getElementById("url");
+const readingsList = document.getElementById("readingsList");
 
 // -- HTML event listeners -- //
-libraryForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    addToLibrary(
-        titleInput.value,
-        urlInput.value
-    );
+libraryForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addToLibrary(titleInput.value, urlInput.value);
 });
 
 // -- Functions that actually do stuff -- //
 
 // This function adds a reading to our library and also renders it
 function addToLibrary(title, url) {
-    console.log('ADD:', title, url);
+  console.log("ADD:", title, url);
 
-    if (!title || !url) {
-        alert('Make sure you provide a title and URL first!');
-        return;
-    }
+  if (!title || !url) {
+    alert("Make sure you provide a title and URL first!");
+    return;
+  }
 
-    const stringReadings = localStorage.getItem('studBudReadings');
-    const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
-    listOfReadings.push({
-        title,
-        url
-    });
-    localStorage.setItem('studBudReadings', JSON.stringify(listOfReadings));
+  const id = new Date().getTime();
 
-    renderItem(title, url);
+  const stringReadings = localStorage.getItem("studBudReadings");
+  const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
+  listOfReadings.push({
+    title,
+    url,
+    id,
+  });
+  localStorage.setItem("studBudReadings", JSON.stringify(listOfReadings));
 
-    libraryForm.reset();
+  renderItem(title, url, id);
+
+  libraryForm.reset();
 }
 
 // This renders the item in HTML, inspired by the example
-function renderItem(title, url) {
-    const item = document.createElement('li');
-    const id = new Date().getTime();
-    item.innerHTML = `<div>
+function renderItem(title, url, id) {
+  const item = document.createElement("li");
+  item.innerHTML = `<div>
         <div class="readingContent">
             <div class="readingTitle">
                 ${title}
@@ -57,27 +56,27 @@ function renderItem(title, url) {
         </div>
     </div>`;
 
-    readingsList.appendChild(item);
+  readingsList.appendChild(item);
 
-    const delButton = document.getElementById(`deleteButton${id}`);
-    delButton.addEventListener('click', event => {
-        item.remove();
+  const delButton = document.getElementById(`deleteButton${id}`);
+  delButton.addEventListener("click", (event) => {
+    item.remove();
 
-        const stringReadings = localStorage.getItem('studBudReadings');
-        const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
-        const index = listOfReadings.find(reading => reading.url === url);
-        listOfReadings.splice(index, 1);
-        localStorage.setItem('studBudReadings', JSON.stringify(listOfReadings));
-    });
+    const stringReadings = localStorage.getItem("studBudReadings");
+    const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
+    const index = listOfReadings.findIndex((reading) => reading.url === url);
+    listOfReadings.splice(index, 1);
+    localStorage.setItem("studBudReadings", JSON.stringify(listOfReadings));
+  });
 }
 
 function rememberPriorItems() {
-    const stringReadings = localStorage.getItem('studBudReadings');
-    const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
-
-    for (const reading of listOfReadings) {
-        renderItem(reading.title, reading.url);
-    }
+  const stringReadings = localStorage.getItem("studBudReadings");
+  const listOfReadings = stringReadings ? JSON.parse(stringReadings) : [];
+  
+  for (const reading of listOfReadings) {
+    renderItem(reading.title, reading.url, reading.id);
+  }
 }
 
 // Call this when the page loads so we make sure to remember what items were previously saved
---
+++ public/javascripts/task-board.js
@@ -0,0 +1,122 @@
+// -- HTML elements -- //
+const taskForm = document.getElementById("taskForm");
+const titleInput = document.getElementById("title");
+const dueDateInput = document.getElementById("dueDate");
+const estCompletionTimeInput = document.getElementById("estCompletionTime");
+const priorityInput = document.getElementById("priority");
+const taskList = document.getElementById("taskList");
+
+// -- HTML event listeners -- //
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  addTask(
+    titleInput.value,
+    dueDateInput.value,
+    estCompletionTimeInput.value,
+    priorityInput.value
+  );
+});
+// -- Functions that actually do stuff -- //
+function addTask(title, dueDate, estCompletionTime, priority) {
+  if (!title) {
+    alert("Make sure you provide a title first!");
+    return;
+  }
+
+  const id = new Date().getTime();
+  const stringTasks = localStorage.getItem("studBudTasks");
+  const listOfTasks = stringTasks ? JSON.parse(stringTasks) : [];
+  listOfTasks.push({
+    title,
+    dueDate,
+    estCompletionTime,
+    priority,
+    id,
+  });
+  localStorage.setItem("studBudTasks", JSON.stringify(listOfTasks));
+
+  renderItem(title, dueDate, estCompletionTime, priority, false, id);
+
+  taskForm.reset();
+}
+
+// This renders the item in HTML, inspired by the example
+function renderItem(title, dueDate, estCompletionTime, priority, checked, id) {
+  const item = document.createElement("li");
+  // TODO: add more to this
+  item.innerHTML = `<div>
+        <div class="taskContent" id="taskContent${id}">
+            <div class="taskTitle">
+                ${title}
+            </div>
+            <div class="additionalDetails">
+                ${dueDate} - ${priority} priority
+            </div>
+        </div>
+        <div class="taskActions">
+            <button type="button" id="deleteButton${id}">
+                Delete
+            </button>
+            <input type="checkbox" id="checkbox${id}">
+        </div>
+    </div>`;
+
+  taskList.appendChild(item);
+
+  const delButton = document.getElementById(`deleteButton${id}`);
+  delButton.addEventListener("click", (event) => {
+    item.remove();
+
+    const stringTasks = localStorage.getItem("studBudTasks");
+    const listOfTasks = stringTasks ? JSON.parse(stringTasks) : [];
+    const index = listOfTasks.findIndex((task) => task.title === title);
+    listOfTasks.splice(index, 1);
+    localStorage.setItem("studBudTasks", JSON.stringify(listOfTasks));
+  });
+
+  const checkbox = document.getElementById(`checkbox${id}`);
+  if (checked) {
+    checkbox.setAttribute("checked", "true");
+  }
+
+  setStrikethrough(id, checked);
+
+  checkbox.addEventListener("change", (event) => {
+    const stringTasks = localStorage.getItem("studBudTasks");
+    const listOfTasks = stringTasks ? JSON.parse(stringTasks) : [];
+    const index = listOfTasks.findIndex((task) => task.title === title);
+    const taskToUpdate = listOfTasks[index];
+    taskToUpdate.checked = !taskToUpdate.checked;
+    localStorage.setItem("studBudTasks", JSON.stringify(listOfTasks));
+    setStrikethrough(id, taskToUpdate.checked);
+  });
+}
+
+function setStrikethrough(id, checked) {
+  const taskContent = document.getElementById(`taskContent${id}`);
+  if (checked) {
+    taskContent.classList.add("strikethrough");
+  } else {
+    taskContent.classList.remove("strikethrough");
+  }
+}
+
+function rememberPriorItems() {
+  const stringTasks = localStorage.getItem("studBudTasks");
+  const listOfTasks = stringTasks ? JSON.parse(stringTasks) : [];
+
+  for (const task of listOfTasks) {
+    renderItem(
+      task.title,
+      task.dueDate,
+      task.estCompletionTime,
+      task.priority,
+      task.checked,
+      task.id
+    );
+  }
+}
+
+// Call this when the page loads so we make sure to remember what items were previously saved
+rememberPriorItems();
